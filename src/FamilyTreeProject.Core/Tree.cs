7bf1e7e14685fa7250f0020250bc5e9b5e0d0079
Charles NUrse <charlesnurse@gmail.com>
2015-07-02 14:52:38 -0700

    Add Tree object

---
+++ src/FamilyTreeProject.Core/Tree.cs
@@ -0,0 +1,19 @@
+﻿//******************************************
+//  Copyright (C) 2014-2015 Charles Nurse  *
+//                                         *
+//  Licensed under MIT License             *
+//  (see included LICENSE)                 *
+//                                         *
+// *****************************************
+
+namespace FamilyTreeProject
+{
+    public class Tree
+    {
+        public int TreeId { get; set; }
+
+        public string Name { get; set; }
+
+        public int OwnerId { get; set; }
+    }
+}
